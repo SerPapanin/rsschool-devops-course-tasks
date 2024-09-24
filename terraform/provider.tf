93f17d92055db1eb104b65e716c10ced409df27e
Sergey Panin <panin.tut@gmail.com>
2024-09-24 23:56:10 +0400

    Add tf S3 backend

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         	   = "panin-rs-school-tf-state"
+    key              	 = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+  }
+}
