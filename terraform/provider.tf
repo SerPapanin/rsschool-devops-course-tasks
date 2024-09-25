b8f01654ec281aba79256bed7841e720a6c45c4f
Sergey Panin <panin.tut@gmail.com>
2024-09-25 11:25:34 +0400

    Initial commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,13 +3,14 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.18.0"
+      #profile = "terraform_user"
     }
   }
 
   backend "s3" {
-    bucket         	   = "panin-rs-school-tf-state"
-    key              	 = "state/terraform.tfstate"
-    region         	   = "us-east-1"
-    encrypt        	   = true
+    bucket  = "panin-rs-school-tf-state"
+    key     = "state/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
