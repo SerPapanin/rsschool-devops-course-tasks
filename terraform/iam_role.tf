712c49127d1fb5375c016923f76159df9c2ed49a
Sergey Panin <panin.tut@gmail.com>
2024-09-28 21:49:31 +0400

    Clean code

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -1,19 +1,10 @@
 provider "aws" {
-  #region = "us-east-1"
 }
 
+# Create GitHub Action Role and assume policy
 resource "aws_iam_role" "GithubActionsRole" {
-  name = "GithubActionsRole"
-  #assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
-    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/IAMFullAccess",
-    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
-    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
-    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
-  ]
+  name                = "GithubActionsRole"
+  managed_policy_arns = github_action_iam_policies
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -30,7 +21,6 @@ resource "aws_iam_role" "GithubActionsRole" {
           },
           "StringLike" : {
             "token.actions.githubusercontent.com:sub" : "repo:SerPapanin/rsschool-devops-course-tasks:*"
-            #https://github.com/SerPapanin/rsschool-devops-course-tasks/
           }
         }
       }
@@ -42,7 +32,7 @@ resource "aws_iam_role" "GithubActionsRole" {
   }
 }
 
-# GitHub Actions OIDC Provider
+# Create GitHub Actions OIDC Provider
 resource "aws_iam_openid_connect_provider" "github_actions_IODC_provider" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
@@ -52,56 +42,3 @@ resource "aws_iam_openid_connect_provider" "github_actions_IODC_provider" {
     Project = "rs-school"
   }
 }
-/*data "aws_iam_policy_document" "instance_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}*/
-
-# List of Managed Policies
-/*locals {
-  managed_policies = [
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
-    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/IAMFullAccess",
-    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
-    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
-    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
-  ]
-}
-
-# Attach all Managed Policies in a loop
-resource "aws_iam_role_policy_attachment" "GithubActionsRole_policy_attachment" {
-  for_each = toset(local.managed_policies)
-
-  role       = aws_iam_role.GithubActionsRole1.name
-  policy_arn = each.value
-}
-*/
-
-/*{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Federated": "arn:aws:iam::440744237104:oidc-provider/token.actions.githubusercontent.com"
-            },
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Condition": {
-                "StringEquals": {
-                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
-                },
-                "StringLike": {
-                    "token.actions.githubusercontent.com:sub": "repo:SerPapanin/*"
-                }
-            }
-        }
-    ]
-}*/
