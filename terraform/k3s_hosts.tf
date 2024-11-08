bdae348f49dc51a8dbe8db97154f2b1f126dc388
Sergey Panin <panin.tut@gmail.com>
2024-11-08 13:31:41 +0400

    Change master-node health check

--- terraform/k3s_hosts.tf
+++ terraform/k3s_hosts.tf
@@ -56,28 +56,39 @@ resource "aws_instance" "k3s_control_plane_rs_school" {
   depends_on = [aws_instance.bastion_host_rs_school]
 }
 
-
-# Null resource to run the SSM send-command
-resource "null_resource" "ssm_command_master_node" {
-  # Only run this command after the EC2 instance is created
+# Wait until instance health check is passed
+resource "null_resource" "wait_for_health_check_master_node" {
   depends_on = [aws_instance.k3s_control_plane_rs_school]
 
   provisioner "local-exec" {
     command = <<-EOT
-      HEALTH_STATUS=$(aws ec2 describe-instance-status --instance-ids ${aws_instance.k3s_control_plane_rs_school.id} --query "InstanceStatuses[0].InstanceStatus.Status" --output text --region ${var.aws_region})
-      if [ "$HEALTH_STATUS" = "ok" ]; then
-        echo "Instance is healthy. Proceeding with SSM command."
-        aws ssm send-command \
+      INSTANCE_ID="${aws_instance.k3s_control_plane_rs_school.id}"
+      STATUS=$(aws ec2 describe-instance-status --instance-ids $INSTANCE_ID --query "InstanceStatuses[0].InstanceStatus.Status" --output text)
+
+      while [ "$STATUS" != "ok" ]; do
+        echo "Waiting for master-node health check to pass..."
+        sleep 10
+        STATUS=$(aws ec2 describe-instance-status --instance-ids $INSTANCE_ID --query "InstanceStatuses[0].InstanceStatus.Status" --output text)
+      done
+      echo "Master-node health check passed!"
+    EOT
+  }
+}
+
+# Null resource to run the SSM send-command
+resource "null_resource" "ssm_command_master_node" {
+  # Only run this command after the EC2 instance is created
+  depends_on = [null_resource.wait_for_health_check_master_node]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      aws ssm send-command \
           --instance-ids ${aws_instance.k3s_control_plane_rs_school.id} \
           --document-name "AWS-RunShellScript" \
           --parameters commands=["curl -o /tmp/deploy_jenkins.sh https://raw.githubusercontent.com/SerPapanin/rsschool-devops-course-tasks/refs/heads/task_4/terraform/jenkins/deploy_jenkins.sh","chmod +x /tmp/deploy_jenkins.sh","/tmp/deploy_jenkins.sh"] \
           --comment "Deploy Jenkins via TF" \
           --region ${var.aws_region}
-      else
-        echo "Instance is not healthy. Skipping SSM command."
-        exit 1
-      fi
-    EOT
+      EOT
   }
 }
 
