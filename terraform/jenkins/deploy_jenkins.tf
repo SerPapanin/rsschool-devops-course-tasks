8ed8e91f186431e51bce1dc3fbeee78b8ce5fe31
Sergey Panin <panin.tut@gmail.com>
2024-11-09 16:11:36 +0400

    Move Jenkins deployment in separete module

--- terraform/jenkins/deploy_jenkins.tf
+++ terraform/jenkins/deploy_jenkins.tf
@@ -1,3 +1,56 @@
+#Prepare nginx reverse proxy config for jenkins and send it to parameter store
+data "template_file" "nginx_jenkins_conf" {
+  template = file("./templates/nginx_jenkins.tpl")
+  vars = {
+    k3s_private_ip = "${aws_instance.k3s_control_plane_rs_school.private_ip}"
+  }
+}
+
+resource "aws_ssm_parameter" "nginx_jenkins_conf" {
+  name  = "/conf/nginx_jenkins_conf"
+  type  = "String"
+  value = data.template_file.nginx_jenkins_conf.rendered
+}
+
+# SSM document to apply config, copy extra files, restart service, and run a final command
+resource "aws_ssm_document" "apply_nginx_jenkins_conf" {
+  name          = "apply_nginx_jenkins_conf_ssm"
+  document_type = "Command"
+  content = jsonencode({
+    schemaVersion = "2.2",
+    description   = "Apply nginx reverse proxy k3s config, copy extra files, restart service, and run post-restart command",
+    mainSteps = [
+      {
+        action = "aws:runShellScript",
+        name   = "applyConfigAndCopyFiles",
+        inputs = {
+          runCommand = [
+            # Retrieve the configuration file and additional files from SSM Parameter Store
+            "CONFIG_JENKINS=$(aws ssm get-parameter --name '/conf/nginx_jenkins_conf' --query 'Parameter.Value' --output text)",
+
+            # Write the configuration and additional files to their destinations on the instance
+            "echo \"$CONFIG_JENKINS\" > /etc/nginx/sites-enabled/jenkins.conf",
+            # Restart nginx service after applying all configuration and files
+            "systemctl restart nginx"
+          ]
+        }
+      }
+    ]
+  })
+}
+
+# Associate the SSM document with bastion host
+resource "aws_ssm_association" "apply_nginx_jenkins_conf_association" {
+  name = aws_ssm_document.apply_nginx_jenkins_conf.name
+  targets {
+    key    = "InstanceIds"
+    values = [aws_instance.bastion_host_rs_school.id]
+  }
+  depends_on = [null_resource.wait_for_health_check_bastion]
+}
+
+
+######## Deploy Jenkins using HELM chart to k3s cluster
 # Wait until instance health check is passed
 resource "null_resource" "wait_for_health_check_master_node" {
   depends_on = [aws_instance.k3s_control_plane_rs_school]
